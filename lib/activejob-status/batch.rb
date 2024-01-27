c4e0f6e3547c356d3039b44d97772768c2ef9c2f
Matija Čupić <matteeyah@gmail.com>
2024-01-27 14:45:30 +0100

    Use read_multi for reading statuses

--- lib/activejob-status/batch.rb
+++ lib/activejob-status/batch.rb
@@ -4,19 +4,30 @@ module ActiveJob
   module Status
     class Batch
       def initialize(jobs)
-        @statuses = jobs.map { |job| ActiveJob::Status.get(job) }
+        @jobs = jobs
+        @storage = ActiveJob::Status::Storage.new
       end
 
       def status
-        if @statuses.all? { |status| status[:status] == :queued }
+        if @jobs.all? { |job| status_for(job) == :queued }
           :queued
-        elsif @statuses.any? { |status| status[:status] == :failed }
+        elsif @jobs.any? { |job| status_for(job) == :failed }
           :failed
-        elsif @statuses.all? { |status| status[:status] == :completed }
+        elsif @jobs.all? { |job| status_for(job) == :completed }
           :completed
         else
           :working
         end
+      end
+
+      private
+
+      def statuses
+        @statuses ||= @storage.read_multi(@jobs)
+      end
+
+      def status_for(job)
+        statuses.dig(@storage.key(job), :status)
       end
     end
   end
